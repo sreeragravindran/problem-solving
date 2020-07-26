fe8833fad65bbde11702c96879ff25b352984d7e
zf58 <sreerag.ravindran@in.tesco.com>
2020-07-26 20:22:06 +0530

    possible combinations of letters of phone number

---
+++ java/src/main/java/problemsolving/strings/PossibleLetterCombinations.java
@@ -0,0 +1,43 @@
+package ProblemSolving.strings;
+
+import com.intellij.ui.ListUtil;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Input: "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+ */
+public class PossibleLetterCombinations {
+    private String[] _letterMap;
+
+    public PossibleLetterCombinations(){
+        this._letterMap = new String[] {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    }
+
+    public List<String> get(String digits) {
+        if(digits.length() == 0){
+            return null;
+        }
+        LinkedList<String> output = new LinkedList<>();
+        output.add("");
+
+        for(int i=0; i < digits.length(); i++){
+            while(output.peek().length() == i){
+                String temp = output.remove();
+                int digit = Integer.parseInt(String.valueOf(digits.charAt(i)));
+                for(char c : _letterMap[digit].toCharArray())
+                    output.add(temp + c);
+            }
+        }
+        return output;
+    }
+}
---
+++ java/src/test/java/problemsolving/strings/PossibleLetterCombinationsTest.java
@@ -0,0 +1,20 @@
+package ProblemSolving.strings;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PossibleLetterCombinationsTest {
+
+    @Test
+    public void should_get_all_combinations_for_23() {
+        PossibleLetterCombinations possibleLetterCombinations = new PossibleLetterCombinations();
+        List<String> actual = possibleLetterCombinations.get("23");
+        List<String> expected = Arrays.asList("ad", "bd", "cd", "ae", "be", "ce", "af", "bf", "cf");
+        Assert.assertTrue(actual.size() == expected.size());
+        Assert.assertTrue(actual.stream().allMatch(e -> expected.contains(e)));
+    }
+}

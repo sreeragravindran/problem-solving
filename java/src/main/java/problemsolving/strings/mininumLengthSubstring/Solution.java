cfe08308bb63b4c503f34846d2d6b9b0bfd75162
zf58 <sreerag.ravindran@in.tesco.com>
2021-04-11 00:15:12 +0530

    minimim sliding window

---
+++ java/src/main/java/problemsolving/strings/mininumLengthSubstring/Solution.java
@@ -0,0 +1,78 @@
+package ProblemSolving.strings.mininumLengthSubstring;
+
+import com.google.gson.internal.$Gson$Preconditions;
+import kotlin.jvm.internal.MagicApiIntrinsics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * this is minimum sliding window problem
+ */
+
+public class Solution {
+
+    // Add any helper functions you may need here
+    public static Map<Character, Integer> createMap(String s){
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i), map.get(s.charAt(i)) +1);
+            } else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+        return map;
+    }
+
+
+    public static boolean contains(
+            Map<Character, Integer> superset,
+            Map<Character, Integer> subset){
+
+        for (Character key: subset.keySet()){
+            if(!superset.containsKey(key) || !superset.get(key).equals(subset.get(key))) {
+                return false;
+            }
+        }
+        return  true;
+    }
+
+    public static int minLengthSubstring(String s, String t) {
+        // Write your code here
+        int leftIndex = 0;
+        int rightIndex = t.length() - 1;
+        int minimumWindow = Integer.MAX_VALUE;
+
+        Map<Character, Integer> t_map = createMap(t);
+        Map<Character, Integer> supersetMap = createMap(s.substring(leftIndex, t.length()));
+
+        while(rightIndex < s.length() && (rightIndex - leftIndex +1) >= t.length()){
+            if(contains(supersetMap, t_map)){
+                if(minimumWindow > (rightIndex - leftIndex + 1)){
+                    minimumWindow = (rightIndex - leftIndex + 1);
+                }
+                int value = supersetMap.get(s.charAt(leftIndex));
+                if(value > 1){
+                    supersetMap.put(s.charAt(leftIndex), value - 1);
+                } else {
+                    supersetMap.remove(s.charAt(leftIndex));
+                }
+                leftIndex++;
+            }
+            else {
+                rightIndex++;
+                if(rightIndex < s.length()){
+                    if(supersetMap.containsKey(s.charAt(rightIndex))){
+                        supersetMap.put(s.charAt(rightIndex), supersetMap.get(s.charAt(rightIndex)) + 1);
+                    } else {
+                        supersetMap.put(s.charAt(rightIndex), 1);
+                    }
+                }
+            }
+        }
+
+        return minimumWindow == Integer.MAX_VALUE ? -1 : minimumWindow;
+    }
+}
---
+++ java/src/test/java/problemsolving/strings/minimumLengthSubstring/MinimumLengthSubstringTests.java
@@ -0,0 +1,26 @@
+package ProblemSolving.strings.minimumLengthSubstring;
+
+import ProblemSolving.strings.mininumLengthSubstring.Solution;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MinimumLengthSubstringTests {
+
+    @Test
+    public void test1() {
+        String s = "dcbefebce";
+        String t = "fd";
+        int expected = 5;
+        int actual = Solution.minLengthSubstring(s,t);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2() {
+        String s = "bfbeadbcbcbfeaaeefcddcccbbbfaaafdbebedddf";
+        String t = "cbccfafebccdccebdd";
+        int expected = -1;
+        int actual = Solution.minLengthSubstring(s,t);
+        Assert.assertEquals(expected, actual);
+    }
+}
